64e00d4d3469ed9efd7e2373b3c9617d42d00f6f
Alessandro Moretto <Alessandro@MacBook-Pro-di-Alessandro.local>
2017-01-10 22:56:56 +0100

    fix observable

--- src/app/services/apiRequester.service.ts
+++ src/app/services/apiRequester.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { Observable } from 'rxjs/observable';
+import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mergeMap';
 import { ConfigProvider } from '../services/config.service';
